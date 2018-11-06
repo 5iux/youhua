ad2a40fabb408522761674a3e5da2fdea86202f9
D.Young <y374671004@gmail.com>
2018-11-06 16:38:04 +0800

    Update youhua.user.js

--- youhua.user.js
+++ youhua.user.js
@@ -12,7 +12,7 @@
 // @match        *://siteadmin.taobao.com/design.htm*
 // @match        *://siteadmin.tmall.com/preview.htm*
 // @match        *://siteadmin.taobao.com/preview.htm*
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require      https://cdnjs.loli.net/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require      https://raw.githubusercontent.com/5iux/youhua/master/youhua.js
 // @grant        none
 // ==/UserScript==

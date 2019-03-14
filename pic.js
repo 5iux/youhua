ce18d2489661f9bec536c767222d69fc5b9a6085
D.Young <13775651+5iux@users.noreply.github.com>
2019-03-14 16:30:21 +0800

    Delete pic.js

--- pic.js
+++
@@ -1,9 +0,0 @@
-$(".wp-topnav-link:last").after('<div class="wp-topnav-link hy_btn01">店铺装修</div><div class="wp-topnav-link hy_btn02">旗舰店店铺装修</div>');
-$(document).ready(function(e) {
-    $(".hy_btn01").click(function(e) {
-      $("[title='店铺装修████']").click();
-    });
-    $(".hy_btn02").click(function(e) {
-      $("[title='旗舰店店铺装修']").click();
-    });
-});

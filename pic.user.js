2da1f1bd67acfb2eb6c26df6d8c0bc3ab088bd3f
D.Young <13775651+5iux@users.noreply.github.com>
2019-03-14 16:18:23 +0800

    Add files via upload

---
+++ pic.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         图片空间跳转
+// @namespace    https://5iux.github.io/youhua
+// @version      2019.3.14
+// @description  图片空间跳转
+// @author       D.Young
+// @match        *://sucai.wangpu.taobao.com*
+// @require      https://raw.githubusercontent.com/5iux/youhua/master/pic.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $("body").before('')
+   
+})();

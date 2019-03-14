7019cc81bd1964bc0e863e70b823894029a9228b
D.Young <13775651+5iux@users.noreply.github.com>
2019-03-14 16:23:21 +0800

    Add files via upload

--- pic.js
+++ pic.js
@@ -1,4 +1,4 @@
-$("body").after('<div class="wp-topnav-link hy_btn01">店铺装修</div><div class="wp-topnav-link hy_btn02">旗舰店店铺装修</div>');
+$(".wp-topnav a:last").after('<div class="wp-topnav-link hy_btn01">店铺装修</div><div class="wp-topnav-link hy_btn02">旗舰店店铺装修</div>');
 $(document).ready(function(e) {
     $(".hy_btn01").click(function(e) {
       $("[title='店铺装修████']").click();

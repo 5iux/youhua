adef1f9541e14515ad3ab8db80034a20202b4c85
D.Young <13775651+5iux@users.noreply.github.com>
2019-03-14 16:34:57 +0800

    Update pic.user.js

--- pic.user.js
+++ pic.user.js
@@ -4,14 +4,14 @@
 // @version      2019.3.14
 // @description  图片空间跳转
 // @author       D.Young
-// @match        *://sucai.wangpu.taobao.com*
+// @match        *sucai.wangpu.taobao.com*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     $(document).ready(function(e) {
-        $(".wp-topnav-link:last").after('<div class="wp-topnav-link hy_btn01">店铺装修</div>');
+        $(".wp-topnav-link:last").after('<a class="wp-topnav-link hy_btn01">店铺装修</a>');
         $(".hy_btn01").click(function(e) {
             $("[title='店铺装修████']").click();
         });

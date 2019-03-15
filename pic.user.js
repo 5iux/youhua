332dcab3c735b52b2c45a7540e9c7d85e9b4b9cf
D.Young <13775651+5iux@users.noreply.github.com>
2019-03-15 14:57:16 +0800

    Update pic.user.js

--- pic.user.js
+++ pic.user.js
@@ -1,20 +1,16 @@
 // ==UserScript==
 // @name         图片空间跳转
 // @namespace    https://5iux.github.io/youhua
-// @version      2019.3.14
+// @version      2019.3.15
 // @description  图片空间跳转
 // @author       D.Young
 // @match        *sucai.wangpu.taobao.com*
+// @require      https://raw.githubusercontent.com/5iux/youhua/master/pic.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    $(document).ready(function(e) {
-        $(".wp-topnav-link:last").after('<a class="wp-topnav-link hy_btn01">店铺装修</a>');
-        $(".hy_btn01").click(function(e) {
-            $("[title='店铺装修████']").click();
-        });
-    });
+    
 
 })();

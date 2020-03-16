47ed6713c5bf2d214ff46f8bbc1d3aa127898f19
5iux <13775651+5iux@users.noreply.github.com>
2020-03-16 15:34:22 +0800

    cdn加速

--- youhua.user.js
+++ youhua.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         韩都淘内优化连接
 // @namespace    https://5iux.github.io/youhua
-// @version      2019.12.26.1
+// @version      2020.03.16
 // @description  本脚本主要针对淘内后台优化，增加快捷方式、优化链接http缺失、一键居中、模块去除10像素间距等；装修后台增加新窗口打开，欢迎交流使用;
 // @author       D.Young
 // @match        *://siteadmin.tmall.com/module*
@@ -14,7 +14,7 @@
 // @match        *://siteadmin.taobao.com/preview.htm*
 // @match        *://login.taobao.com/member/login.jhtml*
 // @require      https://cdnjs.loli.net/ajax/libs/jquery/3.3.1/jquery.min.js
-// @require      https://raw.githubusercontent.com/5iux/youhua/master/youhua.js?t=20191226
+// @require      https://cdn.jsdelivr.net/gh/5iux/youhua@latest/youhua.js
 // @grant        GM_addStyle
 // @run-at       document-end
 // ==/UserScript==

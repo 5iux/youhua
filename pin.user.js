39228c2c600d3480286a95dade2100fbc28f0eeb
5iux <y374671004@gmail.com>
2020-11-17 10:25:49 +0800

    Update pin.user.js

--- pin.user.js
+++ pin.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         pinterest美化
 // @namespace    https://blog.5iux.cn
-// @version      0.1.2
-// @description  简单美化下pinterest,主要针对部分圆角过大调整、字体更换为非衬线体。
+// @version      0.1.3
+// @description  简单美化下pinterest,主要针对部分圆角过大调整、调整部分字体字号，整体更换为等线体。
 // @author       DYoung
 // @match        *://www.pinterest.com*
 // @match        *://www.pinterest.co.uk*
+// @match        *://www.pinterest.co.kr*
 // @match        *://www.pinterest.ca*
+// @match        *://www.pinterest.co*
+// @match        *://co.pinterest.com*
 // @match        *://www.pinterest.co.jp*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js
@@ -27,6 +30,7 @@
        .Jea.XiG.mQ8.zI7.iyn.Hsu{border-radius:10px !important;}\
        .sLG.zI7.iyn.Hsu{border-radius:10px !important;}\
        .XiG.zI7.iyn.Hsu{border-radius:10px !important;}\
+       .Jea.mQ8.ujU.zI7.iyn.Hsu {border-radius: 10px !important;}\
 	</style>');
 
     // Your code here...
